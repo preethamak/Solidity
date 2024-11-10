bac6a4e3214573de62789f26fd878c92b3376471
preethamak <167765989+preethamak@users.noreply.github.com>
2024-11-10 08:08:50 +0530

    Add files via upload

---
+++ DoWhile.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DOwhile{
+    uint256[] data;
+    uint8 j= 0;
+
+    function loop() public returns(uint256[] memory){
+        do{
+            j++;
+            data.push(j);
+        }while(j<5);
+            return data;
+        
+    }
+}
